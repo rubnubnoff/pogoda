e24d6e22052d0222869a9cb7d699c93ca585410b
Alex Rubnubnoff <rubnubnoff@mail.ru>
2019-08-07 15:56:06 +0400

    Updates

---
+++ precache-manifest.04f8b2b890291a7304235880d7ecf5ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/pogoda/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "bdb4c9d2324581a2858d",
+    "url": "/pogoda/static/js/runtime~main.a579aba6.js"
+  },
+  {
+    "revision": "aaf095ef90e3b0448098",
+    "url": "/pogoda/static/js/main.ec6f3882.chunk.js"
+  },
+  {
+    "revision": "130e950a8e70d51225fa",
+    "url": "/pogoda/static/js/2.a61309f7.chunk.js"
+  },
+  {
+    "revision": "aaf095ef90e3b0448098",
+    "url": "/pogoda/static/css/main.025168a1.chunk.css"
+  },
+  {
+    "revision": "130e950a8e70d51225fa",
+    "url": "/pogoda/static/css/2.d4b9ad94.chunk.css"
+  },
+  {
+    "revision": "76a32f41d6a61f77cd2846287480237c",
+    "url": "/pogoda/index.html"
+  }
+];
